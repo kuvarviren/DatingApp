a94b6c99a95b7ddc5c0fd61d8e30a13c9e19ca3f
KUMAR Virendra <VKR1@dsone.3ds.com>
2021-12-22 01:15:21 +0530

    Added Authentication

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -3,14 +3,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private AppDbContext _context;
         public UsersController(AppDbContext context)
@@ -24,6 +23,7 @@ namespace API.Controllers
             return users;
         }
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             var user = await _context.Users.FindAsync(id);
