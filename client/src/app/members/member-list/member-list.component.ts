5e09d63400f59ed4fa523e731e1a76f4b1d58aee
KUMAR Virendra <VKR1@dsone.3ds.com>
2022-01-13 01:49:44 +0530

    restored caching

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -17,32 +17,30 @@ export class MemberListComponent implements OnInit {
   members:Member[];
   pagination:Pagination;
   userParams:UserParams;
-  user:User;
 
   genderList=[{value:"male",display:"Males"},{value:"female",display:"Females"}];
 
-  constructor(private memberService: MembersService, private accountService: AccountService) { 
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
-      this.user=user;
-      this.userParams = new UserParams(user);
-    });
+  constructor(private memberService: MembersService) { 
+      this.userParams = memberService.getUserParams();
   }
 
   ngOnInit(): void {
     this.loadMembers();
   }
   loadMembers(){
+    this.memberService.setUserParams(this.userParams);
     this.memberService.getMembers(this.userParams).subscribe(response=>{
       this.pagination=response.pagination;
       this.members = response.result;
     })
   }
   pageChanged(event:any){
+    this.memberService.setUserParams(this.userParams);
     this.userParams.pageNumber = event.page;
     this.loadMembers();
   }
   resetFilters(){
-    this.userParams = new UserParams(this.user);
+    this.userParams = this.memberService.resetUserParams();
     this.loadMembers();
   }
 }
